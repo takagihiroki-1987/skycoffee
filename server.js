ba2982608737deef07f3873caac21ee6f5639e08
takagihiroki-1987 <113028008+takagihiroki-1987@users.noreply.github.com>
2023-06-08 18:47:49 +0900

    imgを指定

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const http = require("http");
 const PORT = 3000;
 const html = require("fs").readFileSync("./index.html");
+const app = express();
 
 //webサーバー作成
 const server = http.createServer((req, res) => {
@@ -13,3 +14,12 @@ const server = http.createServer((req, res) => {
 server.listen(PORT, () => {
   console.log("server running!");
 });
+
+//'link'の部分はファイルをしている。
+app.use(express.static("assets"));
+
+app.get("/", (req, res) => {
+  res.render(html);
+});
+
+app.listen(3000);
