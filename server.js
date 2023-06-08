5f1a5627529dee239acb3a956e5cf8d7d3a4f009
takagihiroki-1987 <113028008+takagihiroki-1987@users.noreply.github.com>
2023-06-08 18:23:00 +0900

    変更

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
-console.log("node.js");
+const http = require("http");
+const PORT = 3000;
+const html = require("fs").readFileSync("./index.html");
+
+//webサーバー作成
+const server = http.createServer((req, res) => {
+  //ブラウザからアクセスが来た時の処理
+  res.writeHead(200, { "Content-Type": "text/html" });
+  res.write(html);
+  res.end();
+});
+
+server.listen(PORT, () => {
+  console.log("server running!");
+});
